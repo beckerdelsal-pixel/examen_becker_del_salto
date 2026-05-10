9f9503ee55f98f345ed2da632d98b06ab702cd56
beckerdelsal-pixel <beckerdelsal@gmail.com>
2026-05-09 20:30:38 -0500

    Inicializar proyecto Spring Boot y repositorio

---
+++ examen/src/main/java/com/krakedev/examen/entidades/Vendedor.java
@@ -0,0 +1,55 @@
+package com.krakedev.examen.entidades;
+
+public class Vendedor {
+	private String cedula;
+    private int numeroVentas;
+    private double sueldoFijo;
+    private double comisionPorVenta;
+
+    public Vendedor(String cedula) {
+        super();
+        this.cedula = cedula;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public int getNumeroVentas() {
+        return numeroVentas;
+    }
+
+    public void setNumeroVentas(int numeroVentas) {
+        this.numeroVentas = numeroVentas;
+    }
+
+    public double getSueldoFijo() {
+        return sueldoFijo;
+    }
+
+    public void setSueldoFijo(double sueldoFijo) {
+        this.sueldoFijo = sueldoFijo;
+    }
+
+    public double getComisionPorVenta() {
+        return comisionPorVenta;
+    }
+
+    public void setComisionPorVenta(double comisionPorVenta) {
+        this.comisionPorVenta = comisionPorVenta;
+    }
+
+    public double calcularSueldo() {
+        return sueldoFijo;
+    }
+
+    @Override
+    public String toString() {
+        return "Vendedor [cedula=" + cedula + ", numeroVentas=" + numeroVentas + ", sueldoFijo=" + sueldoFijo
+                + ", comisionPorVenta=" + comisionPorVenta + "]";
+    }
+}
