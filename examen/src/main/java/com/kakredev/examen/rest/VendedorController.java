c9376e4f29d701c0573b92f7e9bef0cfe875c544
beckerdelsal-pixel <beckerdelsal@gmail.com>
2026-05-09 22:30:21 -0500

    Correccion metodos para calcular sueldo

--- examen/src/main/java/com/kakredev/examen/rest/VendedorController.java
+++ examen/src/main/java/com/kakredev/examen/rest/VendedorController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.krakedev.examen.entidades.Vendedor;
+import com.krakedev.examen.entidades.VendedorComision;
+import com.krakedev.examen.entidades.VendedorMixto;
 import com.krakedev.examen.servicios.AdminVentas;
 
 @RestController
@@ -18,7 +20,23 @@ public class VendedorController {
 
     @PostMapping("/agregar") 
     public void agregar(@RequestBody Vendedor vendedor) {
-        admin.agregar(vendedor);
+    	Vendedor realVendedor = null;
+        
+        if (vendedor.getTipo().equals("V")) {
+            realVendedor = new Vendedor(vendedor.getCedula(), "V");
+        } else if (vendedor.getTipo().equals("C")) {
+            realVendedor = new VendedorComision(vendedor.getCedula(), "C");
+        } else if (vendedor.getTipo().equals("M")) {
+            realVendedor = new VendedorMixto(vendedor.getCedula(), "M");
+        }
+
+        if (realVendedor != null) {
+            realVendedor.setSueldoFijo(vendedor.getSueldoFijo());
+            realVendedor.setNumeroVentas(vendedor.getNumeroVentas());
+            realVendedor.setComisionPorVenta(vendedor.getComisionPorVenta());
+            
+            admin.agregar(realVendedor);
+        }
     }
 
     @GetMapping("/calcularSueldo/{cedula}") 
